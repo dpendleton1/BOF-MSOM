37abab2fba8ccd8f28708ea045eb3a2c2b838800
Dan Pendleton <daniel.pendleton@gmail.com>
2024-11-18 10:00:11 -0500

    removed section (already commented out) that proposed to make NUMBER binary.

--- bof_data_prep.R
+++ bof_data_prep.R
@@ -1,34 +1,12 @@
 #BOF DATA PREP
 
-rm(list = ls())
+
 library(tidyverse)
 
-## inputs
-#years
-begYEAR = 1987
-endYEAR = 1987
-
-#months
-begMONTH = 8
-endMONTH = 9
-
-#seasons
-source('~/Documents/WorkDocuments/Projects/Fundy/makeSeasons.r')
-# MONTHLY SEASONS
-   #ssn_beg=rbind(c(8,1), c(9,1))
-   #ssn_end=rbind(c(8,31),c(9,30))
-# 2-MONTH SEASONS
-  ssn_beg=rbind(c(8,1))
-  ssn_end=rbind(c(9,30))
-# 2-WEEK SEASONS
-#    ssn_beg=rbind(c(8,1), c(8,16), c(9,1), c(9,16))
-#    ssn_end=rbind(c(8,15), c(8,31), c(9,15), c(9,30))
-# SEASON FOR TESTING
-    #ssn_beg = rbind(c(8,1), c(8,16))
-    #ssn_end = rbind(c(8,15), c(8,31))
+## 0. run bof_param_specification.R
 
 ## 1. import data
-dat <- read_csv(file = "~/Documents/WorkDocuments/Projects/Fundy/Dan & Kelsey All FUNDY data 05-19-2023.CSV", 
+dat <- read_csv(file = file_loc, 
                 col_types = cols(FILEID = col_character(),
                                  EVENTNO = col_double(),
                                  MONTH = col_double(),
