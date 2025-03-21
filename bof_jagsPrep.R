5759cb92c5fe0481520767e08ff5a0d4f9b7838c
Dan Pendleton <daniel.pendleton@gmail.com>
2025-03-21 11:02:28 -0400

    comment / uncomment / cleanup / trivial changes

--- bof_jagsPrep.R
+++ bof_jagsPrep.R
@@ -188,20 +188,18 @@ for (i in 1:num_ssn){
     nereid_tracks = st_sf(nereid_tracks)
     class(nereid_tracks)
 
-    #plot and save only one map (to save space)
-    #if (j == 1){
-      #create the survey map
-      survey_map = mapview(nereid_tracks, color = "red", lwd = 4, alpha = 1, popup = NULL) +
-        mapview(tmpdat_sf_season_survey, color = "blue", cex = 2, alpha = .2, popup = NULL) +
-        mapview(area_grid_sf)
-      survey_map  #plot the survey map
-
-      #write and view map as html file
-      html_fl = paste0(curr_dir, "/figs/", unique(tmpdat_sf_season$YEAR), "_ssn", i, "_surv", j, "_", season_ufids[j], ".html")
-      mapshot(survey_map, url = html_fl) #save the map
-      #browseURL(html_fl) #open the map in a web browser
-
-    #}
+    # #plot and save only one map (to save space)
+    # #if (j == 1){
+    #   #create the survey map
+    #   survey_map = mapview(nereid_tracks, color = "red", lwd = 4, alpha = 1, popup = NULL) +
+    #     mapview(tmpdat_sf_season_survey, color = "blue", cex = 2, alpha = .2, popup = NULL) +
+    #     mapview(area_grid_sf)
+    #   survey_map  #plot the survey map
+    #   #write and view map as html file
+    #   html_fl = paste0(curr_dir, "/figs/", unique(tmpdat_sf_season$YEAR), "_ssn", i, "_surv", j, "_", season_ufids[j], ".html")
+    #   mapshot(survey_map, url = html_fl) #save the map
+    #   #browseURL(html_fl) #open the map in a web browser
+    # #}
     
     #intersect grid with survey trackline (linestring), calculate and store trackline length in each grid cell
       intersection <- st_intersection(area_grid_sf, nereid_tracks) %>%
--- bof_param_specification.R
+++ bof_param_specification.R
@@ -10,7 +10,7 @@ setwd(curr_dir)
 ## inputs
 #years
 begYEAR = 1988
-endYEAR = 1988
+endYEAR = 1995
 
 #months
 begMONTH = 8
